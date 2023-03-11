f1c967e1bf9d7be8ae5840537231c898fa631fd3
Martin Zikmund <martin@zikmund.dev>
2023-03-11 11:18:09 +0100

    Issue repro

---
+++ AppTests/AppTests/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using AppTests.ViewModels;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AppTests
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //global::System.Diagnostics.Debug.WriteLine(ViewModel.ApiKey); // The property is there!
+        }
+
+        public TestViewModel ViewModel { get; } = new();
+    }
+}
---
+++ AppTests/AppTests/ViewModels/TestViewModel.cs
@@ -0,0 +1,11 @@
+﻿#nullable enable
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AppTests.ViewModels;
+
+public sealed partial class TestViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string? _apiKey;
+}
